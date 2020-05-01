8b230187a61efcc1587565d64a9ec8197427db21
Charlton Callender <chacalle@uw.edu>
2020-05-01 13:04:56 -0700

    added package documentation

---
+++ R/packageTemplate.R
@@ -0,0 +1,10 @@
+#' @title ihmeuw-demographics R package template
+#'
+#' @description ihmeuw-demographics R package template that is used when
+#'   creating new R packages for our team.
+#'
+#' @docType package
+#' @name packageTemplate
+#'
+#' @importFrom utils globalVariables
+NULL
